47b52775a52388cf71e6f9b1f5437f408538a0e6
Agnes Beviz <agnes.beviz@gmail.com>
2022-07-30 16:06:17 +0100

    numbers add - error validation in express js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { sayHello, uppercase, lowercase, firstCharacters } = require('./lib/strings.js');
+const { sayHello, uppercase, lowercase, firstCharacters } = require('./lib/strings');
+const { add } = require('./lib/numbers');
 
 const app = express();
 
+// strings
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
 });
@@ -16,11 +18,19 @@ app.get('/strings/lower/:string', (req, res) => {
 });
 
 app.get('/strings/first-characters/:string', (req, res) => {
-  if (req.query.length) {
-    res.json({ result: firstCharacters(req.params.string, req.query.length) });
-  } else {
-    res.json({ result: firstCharacters(req.params.string, 1) });
-  }
+  return req.query.length
+    ? res.json({ result: firstCharacters(req.params.string, req.query.length) })
+    : res.json({ result: firstCharacters(req.params.string, 1) });
+});
+
+// numbers
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  const a = parseInt(req.params.a, 10);
+  const b = parseInt(req.params.b, 10);
+
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).json({ result: add(a, b) });
 });
 
 module.exports = app;
