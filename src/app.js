45e4ebdb75325c80dac2ee17c2f6ab3a226222e8
Agnes Beviz <agnes.beviz@gmail.com>
2022-07-30 13:26:28 +0100

    expressjs app for strings.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings.js');
+const { sayHello, uppercase, lowercase, firstCharacters } = require('./lib/strings.js');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.json({ result: firstCharacters(req.params.string, req.query.length) });
 });
 
 module.exports = app;
