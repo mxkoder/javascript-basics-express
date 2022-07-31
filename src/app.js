c3941be8ca19468409d0d828a925a0b1daae7eae
Agnes Beviz <agnes.beviz@gmail.com>
2022-07-31 23:04:52 +0100

    express js middleware for divide function

--- src/app.js
+++ src/app.js
@@ -70,6 +70,24 @@ app.post('/numbers/multiply', (req, res) => {
   }
 });
 
+app.use(express.json());
+app.post('/numbers/divide', (req, res) => {
+  const a = parseInt(req.body.a, 10);
+  const b = parseInt(req.body.b, 10);
+
+  if (req.body.a === 0) {
+    res.status(200).json({ result: 0 });
+  } else if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (!req.body.a || !req.body.b) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (Number.isNaN(a) || Number.isNaN(b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: divide(a, b) });
+  }
+});
+
 module.exports = app;
 
 /* works!!!!!
